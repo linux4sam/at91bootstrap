e2835c91f2e1d5ff9ccc1e5630f3764ee49f884e
Hong Xu <hong.xu@atmel.com>
2011-06-28 13:35:43 +0800

    5series: Remove redundant code
    Clock has already been switched to Main Oscillator previously.
    We don't need to check whether the clock source is Slow Clock.
    
    Signed-off-by: Hong Xu <hong.xu@atmel.com>

--- driver/pmc.c
+++ driver/pmc.c
@@ -72,14 +72,6 @@ void lowlevel_clock_init()
         while (!(read_pmc(PMC_SR) & AT91C_PMC_MOSCXTS))
             ;
     }
-    /*
-     * After stablization, switch to 12MHz Main Oscillator 
-     */
-    if ((read_pmc(PMC_MCKR) & AT91C_PMC_CSS) == AT91C_PMC_CSS_SLOW_CLK) {
-        write_pmc(PMC_MCKR, AT91C_PMC_CSS_MAIN_CLK | AT91C_PMC_PRES_CLK);
-        while (!(read_pmc(PMC_SR) & AT91C_PMC_MCKRDY))
-            ;
-    }
 #else
     if (!(read_pmc(PMC_SR) & AT91C_PMC_MOSCS)) {
         /*
