be9cdf39cab25dac2bfdd6c8dcc4d9ff66ca0ea6
Wenyou Yang <wenyou.yang@atmel.com>
2012-04-18 18:22:12 +0800

    AT91sama5ek: Remove the Unusal file Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

--- lib/libc.mk
+++ lib/libc.mk
@@ -1,14 +1,12 @@
-# Makefile for AT91-Bootstrap libc directory
+# Makefile for AT91Bootstrap libc directory
 # DIRS				+= libc
 
 LIBC:=$(TOPDIR)/lib/
 
-COBJS-y				+=	$(LIBC)string.o
-COBJS-y				+=	$(LIBC)div00.o
-#COBJS-y				+=	$(LIBC)udiv.o
-COBJS-y				+=	$(LIBC)eabi_utils.o
-COBJS-y				+=	$(LIBC)delay.o
-
-SOBJS-y				+=	$(LIBC)_udivsi3.o
-SOBJS-y				+=	$(LIBC)_umodsi3.o
+COBJS-y		+= $(LIBC)string.o
+COBJS-y		+= $(LIBC)delay.o
+COBJS-y		+= $(LIBC)div00.o
+COBJS-y		+= $(LIBC)eabi_utils.o
+SOBJS-y		+= $(LIBC)_udivsi3.o
+SOBJS-y		+= $(LIBC)_umodsi3.o
 
