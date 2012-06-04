927a76b92092f9f11a429458bb629381eaca9a0f
Wenyou Yang <wenyou.yang@atmel.com>
2012-06-04 16:24:31 +0800

    at91sam9g20ek: add recovery function for this ek
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

--- board/at91sam9g20ek/at91sam9g20ek.h
+++ board/at91sam9g20ek/at91sam9g20ek.h
@@ -81,6 +81,10 @@
  */
 #define CONFIG_SYS_BASE_MCI	AT91C_BASE_MCI
 
+
+#define CONFIG_SYS_RECOVERY_BUTTON_PIN	AT91C_PIN_PA(31)
+#define RECOVERY_BUTTON_NAME	"BP4"
+
 /* function */
 extern void hw_init(void);
 
