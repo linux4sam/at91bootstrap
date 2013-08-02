55bc8e519178003ebd1009a8823451700abb9ac3
Bo Shen <voice.shen@atmel.com>
2013-08-02 18:05:29 +0800

    lib: make use the same style for Makefile
    Signed-off-by: Bo Shen <voice.shen@atmel.com>

--- lib/libc.mk
+++ lib/libc.mk
@@ -1,10 +1,10 @@
 # Makefile for AT91Bootstrap libc directory
 # DIRS				+= libc
 
-LIBC:=$(TOPDIR)/lib/
+LIBC:=$(TOPDIR)/lib
 
-COBJS-y		+= $(LIBC)string.o
-COBJS-y		+= $(LIBC)eabi_utils.o
-COBJS-y		+= $(LIBC)div.o
+COBJS-y		+= $(LIBC)/string.o
+COBJS-y		+= $(LIBC)/eabi_utils.o
+COBJS-y		+= $(LIBC)/div.o
 
 COBJS-$(CONFIG_OF_LIBFDT) += $(LIBC)/fdt.o
