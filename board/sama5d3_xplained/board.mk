ff79629f4ab800115ba1944e1c8612d67b639bb2
Josh Wu <josh.wu@atmel.com>
2014-10-20 10:57:16 +0800

    board.mk: refine the command that check the cortex-a5 support
    This patch can avoid generate a tmp file for the check.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- board/sama5d3_xplained/board.mk
+++ board/sama5d3_xplained/board.mk
@@ -1,5 +1,4 @@
-$(shell $(CC) --target-help > tmp.file)
-gcc_cortexa5=$(shell grep cortex-a5 tmp.file)
+gcc_cortexa5=$(shell $(CC) --target-help | grep cortex-a5)
 
 ifeq (, $(findstring cortex-a5,$(gcc_cortexa5)))
 CPPFLAGS += -DCONFIG_SAMA5D3_XPLAINED
@@ -16,5 +15,3 @@ ASFLAGS += \
 	-DCONFIG_SAMA5D3_XPLAINED \
 	-mcpu=cortex-a5
 endif
-
-$(shell rm tmp.file)
--- board/sama5d3xek/board.mk
+++ board/sama5d3xek/board.mk
@@ -1,5 +1,4 @@
-$(shell $(CC) --target-help > tmp.file)
-gcc_cortexa5=$(shell grep cortex-a5 tmp.file)
+gcc_cortexa5=$(shell $(CC) --target-help | grep cortex-a5)
 
 ifeq (, $(findstring cortex-a5,$(gcc_cortexa5)))
 CPPFLAGS += -DCONFIG_SAMA5D3XEK
@@ -15,5 +14,3 @@ ASFLAGS += \
 	-DCONFIG_SAMA5D3XEK
 	-mcpu=cortex-a5
 endif
-
-$(shell rm tmp.file)
