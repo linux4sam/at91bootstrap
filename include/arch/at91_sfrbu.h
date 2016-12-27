93a2fc1c46465c0ddd776130bd533a892c6c9e8a
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2016-12-27 22:11:25 +0100

    Add SFRBU definitions
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

---
+++ include/arch/at91_sfrbu.h
@@ -0,0 +1,43 @@
+/* ----------------------------------------------------------------------------
+ *         ATMEL Microcontroller Software Support
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2016, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __AT91_SFRBU_H__
+#define __AT91_SFRBU_H__
+
+/*
+ * Register Offset
+ */
+#define SFRBU_DDRBUMCR	0x10	/* DDR BU Mode Control Register */
+
+/*
+ * Register Fields
+ */
+
+/*--- SFRBU_DDRMCR: (offset: 0x10) DDR BU Mode Control Register ---*/
+#define AT91C_DDRBUMCR_BUMEN	(0x01	/* Force DDR_DQ Input Buffer Always On */
+
+#endif /* #ifndef __AT91_SFRBU_H__ */
