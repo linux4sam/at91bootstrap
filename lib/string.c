c3fe990832dfa667f50d8c7f527d74624c239e49
Sergey Organov <sorganov@gmail.com>
2017-05-04 23:58:22 +0300

    Add strstr() standard libc function
    Useful, for example, to override Linux command-line root device
    
    Signed-off-by: Sergey Organov <sorganov@gmail.com>

--- lib/string.c
+++ lib/string.c
@@ -133,6 +133,30 @@ char *strchr(const char *s, int c)
 	return (char *)s;
 }
 
+/* NOTE: This is the simple-minded O(len(s1) * len(s2)) worst-case approach. */
+
+char *strstr(const char *s1, const char *s2)
+{
+	register const char *s = s1;
+	register const char *p = s2;
+
+	do {
+		if (!*p) {
+			return (char *) s1;;
+		}
+		if (*p == *s) {
+			++p;
+			++s;
+		} else {
+			p = s2;
+			if (!*s) {
+				return NULL;
+			}
+			s = ++s1;
+		}
+	} while (1);
+}
+
 void *memchr(void *src, int val, unsigned int cnt)
 {
 	char *p = NULL;
