b95cde49bef435d39dffd8efb00c4ad5ec2e5439
Cyrille Pitchen <cyrille.pitchen@atmel.com>
2017-06-01 18:11:38 +0200

    include: add fls() function - find last bit set
    This patch defines the fls() function and the IS_POWER_OF_TWO() macro.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@atmel.com>

---
+++ include/bitops.h
@@ -0,0 +1,45 @@
+/* ----------------------------------------------------------------------------
+ *         ATMEL Microcontroller Software Support
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2017, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef BITOPS_H_
+#define BITOPS_H_
+
+#define IS_POWER_OF_TWO(n)	((n) && !((n) & ((n) - 1)))
+
+static inline int fls(int value)
+{
+	int i;
+
+	for (i = 31; i >= 0; i--)
+		if (value & (1 << i))
+			return i + 1;
+
+	return 0;
+}
+
+#endif /* BITOPS_H_ */
