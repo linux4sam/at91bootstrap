a899e7ba2d72960a0850f976bf58d61d29af618c
Jochen Friedrich <jochen@scram.de>
2018-05-07 08:10:59 +0000

    Add support for second memory bank on AT91SAM9G45 boards.
    Signed-off-by: Jochen Friedrich <jochen@scram.de>

--- include/fdt.h
+++ include/fdt.h
@@ -33,5 +33,6 @@ extern int check_dt_blob_valid(void *blob);
 extern int fixup_chosen_node(void *blob, char *bootargs);
 extern int fixup_memory_node(void *blob,
 				unsigned int *mem_bank,
+				unsigned int *mem_bank2,
 				unsigned int *mem_size);
 #endif /* #ifndef __FDT_H__ */
