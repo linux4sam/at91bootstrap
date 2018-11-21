3529ca3d584af6efa5fbe1a4594d7300817cd79f
Razvan Stefanescu <razvan.stefanescu@microchip.com>
2018-11-21 16:38:22 +0200

    board/chips.mk: remove check for cortex-a5 support in compiler
    GCC8.2 cross-compiler no longer reports known ARM CPUs on '--target-help'
    output so the check for cortex-a5 string needs to be removed or it will
    prevent setting the correct CPP/AS flags.
    
    Signed-off-by: Razvan Stefanescu <razvan.stefanescu@microchip.com>
    Reviewed-by: Eugen Hristev <eugen.hristev@microchip.com>

--- board/chips.mk
+++ board/chips.mk
@@ -69,9 +69,6 @@ ASFLAGS += -mcpu=arm926ej-s -mtune=arm926ej-s -mfloat-abi=soft
 endif
 
 ifeq ($(CORE_CORTEX_A5), y)
-gcc_cortexa5=$(shell $(CC) --target-help | grep cortex-a5)
-ifneq (, $(findstring cortex-a5,$(gcc_cortexa5)))
 CPPFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
 ASFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
-endif
 endif
