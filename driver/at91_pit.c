5ccf367db2f517fda41ea1389ac175ea899b7129
Tudor Ambarus <tudor.ambarus@microchip.com>
2019-01-15 16:26:36 +0200

    driver: at91_pit: comply with naming convention for sam9x60
    s/AT91SAM9X60/SAM9X60
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- driver/at91_pit.c
+++ driver/at91_pit.c
@@ -57,7 +57,7 @@ int timer_init(void)
 #ifdef AT91C_ID_PIT
 	pmc_enable_periph_clock(AT91C_ID_PIT);
 #else
-#if defined(AT91SAM9X60)
+#if defined(SAM9X60)
 	pmc_sam9x5_enable_periph_clk(AT91C_ID_SYS);
 #else
 	pmc_enable_periph_clock(AT91C_ID_SYS);
