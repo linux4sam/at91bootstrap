e36f9ad6ebd9f8e246c0acdede6742bd5ffb9be8
Tudor Ambarus <tudor.ambarus@microchip.com>
2019-03-01 18:56:28 +0200

    scripts: generate pmecc header for sam9x60's nand flash
    MT29F4G08BABWP has:
    - 2048 blocks
    - block size: 256K
    - page size: 4096 bytes
    - oob size: 224
    - bus width: 8 bit
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- scripts/addpmecchead.py
+++ scripts/addpmecchead.py
@@ -17,6 +17,8 @@ elif sys.argv[3] == "sama5d4ek" or sys.argv[3] == "sama5d4_xplained":
 	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
 elif sys.argv[3] == "at91sam9x5ek" or sys.argv[3] == "at91sam9n12ek":
 	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
+elif sys.argv[3] == "sam9x60ek":
+	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
 elif sys.argv[3] == "core9g25":
 	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
 else:
