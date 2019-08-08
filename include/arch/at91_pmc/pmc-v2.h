8ba360be040fe21a157c82effecc60c374e1ce14
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-08-08 18:39:08 +0300

    include: at91_pmc: sam9x60: add remaining defines
    Add remaining defines for PMC.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>

--- include/arch/at91_pmc/pmc-v2.h
+++ include/arch/at91_pmc/pmc-v2.h
@@ -66,5 +66,56 @@
 #define AT91C_PLL_UPDT_STUPTIM	(0xFFUL << 16)
 #define AT91C_PLL_UPDT_STUPTIM_(x) ((x) << 16)
 
+#define PMC_MCKR		0x28		/* Master Clock Register */
+#define AT91C_PMC_CSS		(0x3UL <<  0)
+#define 	AT91C_PMC_CSS_SLOW_CLK		(0x0UL)
+#define 	AT91C_PMC_CSS_MAIN_CLK		(0x1UL)
+#define 	AT91C_PMC_CSS_PLLA_CLK		(0x2UL)
+#define 	AT91C_PMC_CSS_UPLL_CLK		(0x3UL)
+#define	AT91C_PMC_PRES		(0x7UL << 4)
+#define		AT91C_PMC_PRES_CLK		(0x0UL << 4)
+#define		AT91C_PMC_PRES_CLK_2		(0x1UL << 4)
+#define		AT91C_PMC_PRES_CLK_4		(0x2UL << 4)
+#define		AT91C_PMC_PRES_CLK_8		(0x3UL << 4)
+#define		AT91C_PMC_PRES_CLK_16		(0x4UL << 4)
+#define		AT91C_PMC_PRES_CLK_32		(0x5UL << 4)
+#define		AT91C_PMC_PRES_CLK_64		(0x6UL << 4)
+#define		AT91C_PMC_PRES_CLK_3		(0x7UL << 4)
+#define AT91C_PMC_MDIV		(0x3UL <<  8)
+#define 	AT91C_PMC_MDIV_1		(0x0UL <<  8)
+#define 	AT91C_PMC_MDIV_2		(0x1UL <<  8)
+#define 	AT91C_PMC_MDIV_4		(0x2UL <<  8)
+#define 	AT91C_PMC_MDIV_3		(0x3UL <<  8)
+#define AT91C_PMC_PLLADIV2	(0x0)
+#define AT91C_PMC_H32MXDIV	(0x0)
+
+#define PMC_PCR			0x88		/* Peripheral Control Register */
+#define	AT91C_PMC_PID		(0x7FUL << 0)
+#define	AT91C_PMC_GCKCSS	(0x1FUL << 8)
+#define		AT91C_PMC_GCKCSS_MD_SLOW_CLK	(0x0UL << 8)
+#define		AT91C_PMC_GCKCSS_SLOW_CLK	(0x1UL << 8)
+#define		AT91C_PMC_GCKCSS_MAIN_CLK	(0x2UL << 8)
+#define		AT91C_PMC_GCKCSS_MCK_CLK	(0x3UL << 8)
+#define		AT91C_PMC_GCKCSS_PLLA_CLK	(0x4UL << 8)
+#define		AT91C_PMC_GCKCSS_UPLL_CLK	(0x5UL << 8)
+#define	AT91C_PMC_DIV		(0x0)
+#define	AT91C_PMC_GCKDIV	(0xFFUL << 20)
+#define		AT91C_PMC_GCKDIV_MSK		0xFF
+#define		AT91C_PMC_GCKDIV_OFFSET		20
+#define		AT91C_PMC_GCKDIV_(x)		(((x) & AT91C_PMC_GCKDIV_MSK) << AT91C_PMC_GCKDIV_OFFSET)
+#define	AT91C_PMC_EN		(0x1UL << 28)
+#define	AT91C_PMC_GCKEN		(0x1UL << 29)
+#define	AT91C_PMC_CMD		(0x1UL << 31)
+
+#define PMC_GCSR0		0xC0		/* Generic Clock Status Register 0 */
+#define PMC_GCSR1		0xC4		/* Generic Clock Status Register 1 */
+#define PMC_GCSR2		0xC8		/* Generic Clock Status Register 2 */
+#define PMC_GCSR3		0xCC		/* Generic Clock Status Register 3 */
+
+#define PMC_PLL_ISR0		0xEC		/* PLL Interrupt Status Register 0 */
+#define AT91C_PLL_ISR0_LOCKA	(0x1UL << 0)
+
+#define PMC_PLL_ISR1		0xF0		/* PLL Interrupt Status Register 1 */
+
 #endif
 
