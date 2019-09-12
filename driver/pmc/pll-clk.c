linux4sam/at91bootstrap

commit 144f474721952add94e6d268ad36708cdbd6b7a8

driver: pmc: move pll code into its own file
Move PMC PLL code into its own file for better integration w/
future SoCs. SAM9X60 PLL is not touched at the moment.

Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>

driver/pmc/pll-clk.c
@@ -1,0 +1,78 @@
+/* ----------------------------------------------------------------------------
+ *         Microchip Microprocessor (MPU) Software Team
+ * ----------------------------------------------------------------------------
+ * Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Microchip's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY MICROCHIP "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL MICROCHIP BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "board.h"
+#include "common.h"
+#include "div.h"
+#include "arch/at91_pmc/pmc.h"
+
+void pmc_init_pll(unsigned int pmc_pllicpr)
+{
+#ifdef PMC_PLLICPR
+	write_pmc(PMC_PLLICPR, pmc_pllicpr);
+#endif
+}
+
+int pmc_cfg_plla(unsigned int pmc_pllar)
+{
+	/* Always disable PLL before configuring it */
+	write_pmc((unsigned int)PMC_PLLAR, 0 | AT91C_CKGR_SRCA);
+	write_pmc((unsigned int)PMC_PLLAR, pmc_pllar);
+
+	while (!(read_pmc(PMC_SR) & AT91C_PMC_LOCKA))
+		;
+
+	return 0;
+}
+
+unsigned int pmc_get_plla_freq(void)
+{
+	unsigned int tmp;
+	unsigned int divider, multiplier;
+	unsigned int main_clock;
+	unsigned int freq;
+
+#ifdef BOARD_MAINOSC
+	main_clock = BOARD_MAINOSC;
+#else
+	return 0;
+#endif
+
+	tmp = read_pmc(PMC_PLLAR);
+	divider = tmp & AT91C_CKGR_DIVA_MSK;
+	multiplier = (tmp >> AT91C_CKGR_MULA_OFFSET) & AT91C_CKGR_MULA_MSK;
+	if (divider && multiplier) {
+		freq = div(main_clock, divider);
+		freq *= multiplier + 1;
+
+		if (read_pmc(PMC_MCKR) & AT91C_PMC_PLLADIV2)
+			freq /= 2;
+	} else {
+		freq = 0;
+	}
+
+	return freq;
+}
+

Config.in
@@ -568,5 +568,14 @@ config CONFIG_PMC_UTMI_CLK
 	default y
 	help
 	  Enable PMC UTMI clock control
+
+config CONFIG_PMC_PLL_CLK
+	bool "PMC PLL clock control support"
+	depends on AT91SAM9260 || AT91SAM9261 || AT91SAM9263 || AT91SAM9G10 || \
+		AT91SAM9G20 || AT91SAM9G45 || AT91SAM9N12 || AT91SAM9RL || \
+		AT91SAM9X5 || AT91SAM9XE || SAMA5D4 || SAMA5D3X || SAMA5D2
+	default y
+	help
+	  Enable PMC PLL clock control
 endmenu
 

driver/driver.mk
@@ -15,6 +15,7 @@ COBJS-$(CONFIG_PMC_PERIPH_CLK_AT91SAM9) += $(DRIVERS_SRC)/pmc/periph-clk-at91sam
 COBJS-$(CONFIG_PMC_PERIPH_CLK_SAM9X5)	+= $(DRIVERS_SRC)/pmc/periph-clk-sam9x5.o
 COBJS-$(CONFIG_PMC_GENERIC_CLK)	+= $(DRIVERS_SRC)/pmc/generic-clk.o
 COBJS-$(CONFIG_PMC_UTMI_CLK)	+= $(DRIVERS_SRC)/pmc/utmi-clk.o
+COBJS-$(CONFIG_PMC_PLL_CLK)	+= $(DRIVERS_SRC)/pmc/pll-clk.o
 COBJS-$(CONFIG_PIT)		+= $(DRIVERS_SRC)/at91_pit.o
 COBJS-$(CONFIG_WDT)		+= $(DRIVERS_SRC)/at91_wdt.o
 COBJS-$(CONFIG_WDT2)		+= $(DRIVERS_SRC)/at91_wdt2.o

driver/driver_cpp.mk
@@ -172,6 +172,10 @@ ifeq ($(CONFIG_PMC_UTMI_CLK),y)
 CPPFLAGS += -DCONFIG_PMC_UTMI_CLK
 endif
 
+ifeq ($(CONFIG_PMC_PLL_CLK),y)
+CPPFLAGS += -DCONFIG_PMC_PLL_CLK
+endif
+
 ifeq ($(CONFIG_PMECC_CORRECT_BITS_2), y)
 CPPFLAGS += -DPMECC_ERROR_CORR_BITS=2
 endif

driver/pmc/common.c
@@ -156,23 +156,6 @@ void lowlevel_clock_init()
 	return;
 }
 
-void pmc_init_pll(unsigned int pmc_pllicpr)
-{
-	write_pmc(PMC_PLLICPR, pmc_pllicpr);
-}
-
-int pmc_cfg_plla(unsigned int pmc_pllar)
-{
-	/* Always disable PLL before configuring it */
-	write_pmc((unsigned int)PMC_PLLAR, 0 | AT91C_CKGR_SRCA);
-	write_pmc((unsigned int)PMC_PLLAR, pmc_pllar);
-
-	while (!(read_pmc(PMC_SR) & AT91C_PMC_LOCKA))
-		;
-
-	return 0;
-}
-
 int pmc_cfg_mck(unsigned int pmc_mckr)
 {
 	unsigned int tmp;

driver/pmc/generic-clk.c
@@ -105,36 +105,6 @@ int pmc_enable_generic_clock(unsigned int periph_id, unsigned int clk_source,
 	return 0;
 }
 
-static unsigned int pmc_get_plla_freq(void)
-{
-	unsigned int tmp;
-	unsigned int divider, multiplier;
-	unsigned int main_clock;
-	unsigned int freq;
-
-#ifdef BOARD_MAINOSC
-	main_clock = BOARD_MAINOSC;
-#else
-	return 0;
-#endif
-
-	tmp = read_pmc(PMC_PLLAR);
-	divider = tmp & AT91C_CKGR_DIVA_MSK;
-	multiplier = (tmp >> AT91C_CKGR_ALT_MULA_OFFSET)
-					& AT91C_CKGR_ALT_MULA_MSK;
-	if (divider && multiplier) {
-		freq = div(main_clock, divider);
-		freq *= multiplier + 1;
-
-		if (read_pmc(PMC_MCKR) & AT91C_PMC_PLLADIV2)
-			freq /= 2;
-	} else {
-		freq = 0;
-	}
-
-	return freq;
-}
-
 unsigned int pmc_get_generic_clock(unsigned int periph_id)
 {
 	unsigned int tmp;
@@ -156,9 +126,11 @@ unsigned int pmc_get_generic_clock(unsigned int periph_id)
 		freq = 0;
 #endif
 		break;
+#ifdef CONFIG_PMC_PLL_CLK
 	case AT91C_PMC_GCKCSS_PLLA_CLK:
 		freq = pmc_get_plla_freq();
 		break;
+#endif
 	case AT91C_PMC_GCKCSS_UPLL_CLK:
 		freq = 480000000;
 		break;

include/arch/at91_pmc/pmc-v1.h
@@ -40,6 +40,30 @@
 #define		AT91C_CKGR_BIASEN_ENABLED	(0x1UL << 24)
 #define AT91C_CKGR_BIASCOUNT			(0xFUL << 28)
 
+#define PMC_PLLAR		0x28		/* PLL A Register */
+#define AT91C_CKGR_DIVA		(0x1 << 0)
+#define		AT91C_CKGR_DIVA_MSK		0x1
+#define		AT91C_CKGR_DIVA_OFFSET		0
+#define		AT91C_CKGR_DIVA_0		0x0
+#define		AT91C_CKGR_DIVA_BYPASS		0x1
+#define AT91C_CKGR_PLLACOUNT	(0x3FUL << 8)
+#define AT91C_CKGR_OUTA		(0xFUL << 14)
+#define		AT91C_CKGR_OUTA_0		(0x0UL << 14)
+#define		AT91C_CKGR_OUTA_1		(0x1UL << 14)
+#define		AT91C_CKGR_OUTA_2		(0x2UL << 14)
+#define		AT91C_CKGR_OUTA_3		(0x3UL << 14)
+#define AT91C_CKGR_MULA		(0x7f << 18)
+#define		AT91C_CKGR_MULA_MSK		0x7f
+#define		AT91C_CKGR_MULA_OFFSET		18
+#define AT91C_CKGR_SRCA		(0x1UL << 29)
+
+#define PMC_PLLICPR		0x80		/* PLL Charge Pump Current Register */
+#define AT91C_PMC_IPLLA		(0x7UL << 8)
+#define		AT91C_PMC_IPLLA_0		(0x0UL << 8)
+#define		AT91C_PMC_IPLLA_1		(0x1UL << 8)
+#define		AT91C_PMC_IPLLA_2		(0x2UL << 8)
+#define		AT91C_PMC_IPLLA_3		(0x3UL << 8)
+
 #define PMC_PCR			0x10C		/* Peripheral Control Register */
 #define	AT91C_PMC_PID		(0x3F << 0)
 #define	AT91C_PMC_GCKCSS	(0x3 << 8)

include/arch/at91_pmc/pmc.h
@@ -41,8 +41,6 @@
 /* 0x0C Reversed */
 #define PMC_MOR		0x20	/* Main Oscillator Register */
 #define PMC_MCFR	0x24	/* Main Clock  Frequency Register */
-#define PMC_PLLAR	0x28	/* PLL A Register */
-#define PMC_PLLBR	0x2C	/* PLL B Register */
 #define PMC_MCKR	0x30	/* Master Clock Register */
 /* 0x34 Reserved */
 #define PMC_USB		0x38	/* USB clock register */
@@ -59,8 +57,6 @@
 #define PMC_FSPR	0x74	/* Fast Startup Polarity Register */
 #define PMC_FOCR	0x78	/* Fault Output Clear Register */
 /* 0x7C Reserved */
-#define PMC_PLLICPR	0x80	/* PLL Charge Pump Current Register */
-/* 0x84 ~ 0xE0 Reserved */
 #define PMC_WPMR	0xE4	/* Write Protect Mode Register */
 #define PMC_WPSR	0xE8	/* Write Protect Status Register */
 /* 0xEC ~ 0xF8 */
@@ -109,25 +105,6 @@
 #define AT91C_CKGR_CCSS         (0x1UL << 24)
 #define		AT91C_CKGR_CCSS_RC_OSC		(0x0UL << 24)
 #define		AT91C_CKGR_CCSS_XTAL_OSC	(0x1UL << 24)
-
-
-/* -------- CKGR_PLLAR : (PMC Offset: 0x28) PLL A Register --------*/ 
-#define AT91C_CKGR_DIVA		(0xff << 0)
-#define		AT91C_CKGR_DIVA_MSK		0xff
-#define		AT91C_CKGR_DIVA_OFFSET		0
-#define		AT91C_CKGR_DIVA_0		0x0
-#define		AT91C_CKGR_DIVA_BYPASS		0x1
-#define AT91C_CKGR_PLLACOUNT	(0x3FUL <<  8)
-#define AT91C_CKGR_OUTA		(0x3UL << 14)
-#define 	AT91C_CKGR_OUTA_0		(0x0UL << 14)
-#define 	AT91C_CKGR_OUTA_1		(0x1UL << 14)
-#define 	AT91C_CKGR_OUTA_2		(0x2UL << 14)
-#define 	AT91C_CKGR_OUTA_3		(0x3UL << 14)
-#define AT91C_CKGR_MULA		(0xff << 16)
-#define	AT91C_CKGR_ALT_MULA	(0x7f << 18)
-#define		AT91C_CKGR_ALT_MULA_MSK		0x7f
-#define		AT91C_CKGR_ALT_MULA_OFFSET	18
-#define AT91C_CKGR_SRCA		(0x1UL << 29)
 
 /* -------- CKGR_PLLBR : (PMC Offset: 0x2c) PLL B Register --------*/
 #define AT91C_CKGR_DIVB		(0xFFUL <<  0)
@@ -227,17 +204,6 @@
 
 /* -------- PMC_FOCR : (PMC Offset: 0x78) Fault Output Clear Register --------*/ 
 #define AT91C_PMC_FOCLR		(0x1UL << 0)
-
-/* -------- PMC_PLLICPR : (PMC Offset: 0x80) PLL Charge Pump Current Register --------*/ 
-#define AT91C_PMC_ICPPLLA	(0xFUL <<  0)
-#define 	AT91C_PMC_ICPPLLA_0		(0x0UL << 0)
-#define 	AT91C_PMC_ICPPLLA_1		(0x1UL << 0)
-#define AT91C_PMC_REALLOCK	(0x1UL <<  7)
-#define AT91C_PMC_IPLLA		(0xFUL <<  8)
-#define 	AT91C_PMC_IPLLA_0		(0x0UL <<  8)
-#define 	AT91C_PMC_IPLLA_1		(0x1UL <<  8)
-#define 	AT91C_PMC_IPLLA_2		(0x2UL <<  8)
-#define 	AT91C_PMC_IPLLA_3		(0x3UL <<  8)
 
 /* -------- PMC_FEATURES : (PMC Offset: 0xf8)   --------*/ 
 #define AT91C_PMC_CFGAHBCLK	(0x1UL <<  0)

commit d15b9de0ba33102460d90ce80e08d721ac765795

driver: pmc: pll-clk: remove unnecessary #ifdef
#ifdef PMC_PLLICPR is not needed anymore since the driver is selected
only by SoCs that supports it.

Fixes: 144f47472195 ("driver: pmc: move pll code into its own file")
Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

driver/pmc/pll-clk.c
@@ -30,9 +30,7 @@
 
 void pmc_init_pll(unsigned int pmc_pllicpr)
 {
-#ifdef PMC_PLLICPR
 	write_pmc(PMC_PLLICPR, pmc_pllicpr);
-#endif
 }
 
 int pmc_cfg_plla(unsigned int pmc_pllar)