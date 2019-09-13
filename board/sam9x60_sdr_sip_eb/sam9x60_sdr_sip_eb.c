linux4sam/at91bootstrap

commit 7c62e974cc935c2ac0f07c97032bd803f76b643d

board: sam9x60_sdr_sip_eb: introduce SAM9X60 SDR SIP Engineering board
This board features the SAM9X60 SDR SIP with 8MB of external RAM.

Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

board/sam9x60_sdr_sip_eb/sam9x60_sdr_sip_eb.c
@@ -1,0 +1,324 @@
+/* ----------------------------------------------------------------------------
+ *         Microchip Microprocessor (MPU) Software Team
+ * ----------------------------------------------------------------------------
+ * Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Microchip's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY MICROCHIP "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL MICROCHIP BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "common.h"
+#include "hardware.h"
+#include "arch/at91_sfr.h"
+#include "arch/at91_rstc.h"
+#include "arch/at91_pmc/pmc.h"
+#include "arch/at91_smc.h"
+#include "arch/at91_pio.h"
+#include "arch/at91_sdramc.h"
+#include "gpio.h"
+#include "pmc.h"
+#include "usart.h"
+#include "debug.h"
+#include "sdramc.h"
+#include "timer.h"
+#include "watchdog.h"
+#include "string.h"
+#include "sam9x60_sdr_sip_eb.h"
+#include "board_hw_info.h"
+#include "twi.h"
+#include "flexcom.h"
+#include "board.h"
+
+#define PLLA_DIV 1
+#define PLLA_COUNT 0x3f
+#define PLLA_LOOP_FILTER 0
+#define PLLA_CLOCK 200000000
+#define PLLA_FRACR(_p, _q) \
+	((unsigned int)((((unsigned long)(_p)) << 22) / (_q)))
+
+static void at91_dbgu_hw_init(void)
+{
+	/* Configure DBGU pins */
+	const struct pio_desc dbgu_pins[] = {
+		{"RXD", AT91C_PIN_PA(9), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"TXD", AT91C_PIN_PA(10), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_A},
+	};
+
+	pio_configure(dbgu_pins);
+
+	pmc_enable_periph_clock(AT91C_ID_DBGU);
+}
+
+static void initialize_dbgu(void)
+{
+	at91_dbgu_hw_init();
+	usart_init(BAUDRATE(MASTER_CLOCK, BAUD_RATE));
+}
+
+#ifdef CONFIG_SDRAM
+
+static void sdramc_reg_config(struct sdramc_register *sdramc_config)
+{
+	sdramc_config->cr = AT91C_SDRAMC_NC_8
+	| AT91C_SDRAMC_NR_12
+	| AT91C_SDRAMC_CAS_3
+	| AT91C_SDRAMC_NB_4_BANKS
+	| AT91C_SDRAMC_DBW_16_BITS
+	| AT91C_SDRAMC_TWR_3
+	| AT91C_SDRAMC_TRC_11
+	| AT91C_SDRAMC_TRP_3
+	| AT91C_SDRAMC_TRCD_3
+	| AT91C_SDRAMC_TRAS_8
+	| AT91C_SDRAMC_TXSR_12;
+
+        sdramc_config->tr = (MASTER_CLOCK * 7) / 1000000;
+        sdramc_config->mdr = AT91C_SDRAMC_MD_SDRAM
+			 | AT91C_SDRAMC_MD_SHIFT_SAMPLING_2_CYCLE;
+
+	sdramc_config->cfr1 = AT91C_SDRAMC_CFR1_TMRD_2 | AT91C_SDRAMC_CFR1_UNAL
+				| AT91C_SDRAMC_CFR1_ADD_DATA_MUX_UNSUPPORTED
+				| AT91C_SDRAMC_CFR1_CMD_MUX_UNSUPPORTED;
+}
+
+static void sdramc_init(void)
+{
+	struct sdramc_register sdramc_reg;
+	unsigned int reg = 0;
+
+	reg = readl(AT91C_BASE_SFR + SFR_DDRCFG);
+	reg |= (AT91C_EBI_CS1A | AT91C_EBI_DDR_MP_EN);
+	writel(reg, (AT91C_BASE_SFR + SFR_DDRCFG));
+
+	pmc_enable_periph_clock(AT91C_ID_SDRAMC);
+	pmc_enable_system_clock(AT91C_PMC_DDR);
+
+	sdramc_reg_config(&sdramc_reg);
+
+	sdramc_initialize(&sdramc_reg, AT91C_BASE_CS1);
+}
+#endif /* CONFIG SDRAM */
+
+static void at91_green_led_on(void)
+{
+	pio_set_gpio_output(AT91C_PIN_PB(13), 0);
+	pio_set_gpio_output(AT91C_PIN_PB(11), 0);
+	pio_set_gpio_output(AT91C_PIN_PB(12), 1);
+}
+
+#if defined(CONFIG_FLEXCOM)
+unsigned int at91_flexcom0_init(void)
+{
+	const struct pio_desc flx_pins[] = {
+		{"FLX_IO0", AT91C_PIN_PA(0), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"FLX_IO1", AT91C_PIN_PA(1), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_A},
+	};
+
+	pio_configure(flx_pins);
+	pmc_enable_periph_clock(AT91C_ID_FLEXCOM0);
+
+	flexcom_init(0);
+
+	return flexcom_get_regmap(0);
+}
+#endif
+
+#if defined CONFIG_TWI
+void twi_init()
+{
+#if defined(CONFIG_FLEXCOM)
+       twi_bus_init(at91_flexcom0_init);
+#endif
+#if defined(CONFIG_AUTOCONFIG_TWI_BUS)
+	dbg_loud("Auto-Config the TWI Bus by the board\n");
+	at91_board_config_twi_bus();
+#endif
+}
+#endif
+
+#if defined(CONFIG_AUTOCONFIG_TWI_BUS)
+void at91_board_config_twi_bus(void)
+{
+	at24xx_twi_bus = 0;
+}
+#endif
+#if defined CONFIG_FLEXCOM
+static struct at91_flexcom flexcoms[] = {
+#if defined CONFIG_TWI
+	{AT91C_ID_FLEXCOM0, FLEXCOM_TWI, AT91C_BASE_FLEXCOM0, 0},
+#endif
+};
+
+void board_flexcoms_init()
+{
+	flexcoms_init(flexcoms);
+}
+#endif
+
+#ifdef CONFIG_HW_INIT
+void hw_init(void)
+{
+	struct pmc_pll_cfg plla_config;
+
+	/* Disable watchdog */
+	at91_disable_wdt();
+
+	/* Green Led ON */
+	at91_green_led_on();
+
+	/* Configure & Enable PLLA */
+	plla_config.mul = 49;
+	plla_config.div = PLLA_DIV;
+	plla_config.count = PLLA_COUNT;
+	plla_config.fracr = 0;
+	plla_config.loop_filter = PLLA_LOOP_FILTER;
+	pmc_sam9x60_cfg_pll(PLL_ID_PLLA, &plla_config);
+
+	pmc_cfg_mck(BOARD_PRESCALER_PLLA);
+
+	/* Initialize dbgu */
+	initialize_dbgu();
+
+	/* Enable External Reset */
+	writel(AT91C_RSTC_KEY_UNLOCK | AT91C_RSTC_URSTEN, AT91C_BASE_RSTC + RSTC_RMR);
+
+	/* Init timer */
+	timer_init();
+
+#ifdef CONFIG_FLEXCOM
+	board_flexcoms_init();
+#endif
+
+#ifdef CONFIG_TWI
+	twi_init();
+#endif
+
+#ifdef CONFIG_SDRAM
+	/* Initialize SDRAM Controller */
+	sdramc_init();
+#endif
+}
+#endif /* #ifdef CONFIG_HW_INIT */
+
+#ifdef CONFIG_DATAFLASH
+
+#if defined(CONFIG_QSPI)
+void at91_qspi_hw_init(void)
+{
+	const struct pio_desc qspi_pins[] = {
+		{"QSPI0_SCK", AT91C_PIN_PB(19), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"QSPI0_CS", AT91C_PIN_PB(20), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"QSPI0_IO0", AT91C_PIN_PB(21), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"QSPI0_IO1", AT91C_PIN_PB(22), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"QSPI0_IO2", AT91C_PIN_PB(23), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"QSPI0_IO3", AT91C_PIN_PB(24), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_A},
+	};
+
+	pio_configure(qspi_pins);
+
+	pmc_enable_system_clock(AT91C_PMC_QSPICLK);
+	pmc_enable_periph_clock(CONFIG_SYS_ID_QSPI);
+}
+#endif  /* #ifdef CONFIG_QSPI */
+
+#endif	/* #ifdef CONFIG_DATAFLASH */
+
+#ifdef CONFIG_SDCARD
+#ifdef CONFIG_OF_LIBFDT
+void at91_board_set_dtb_name(char *of_name)
+{
+	strcpy(of_name, "at91-sam9x60ek.dtb");
+}
+#endif
+
+#define ATMEL_SDHC_GCKDIV_VALUE     5
+
+void at91_sdhc_hw_init(void)
+{
+#ifdef CONFIG_SDHC0
+	const struct pio_desc sdmmc_pins[] = {
+		{"SDMMC0_CMD",	AT91C_PIN_PA(16), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"SDMMC0_CK",	AT91C_PIN_PA(17), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"SDMMC0_DAT0",	AT91C_PIN_PA(15), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"SDMMC0_DAT1",	AT91C_PIN_PA(18), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"SDMMC0_DAT2",	AT91C_PIN_PA(19), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"SDMMC0_DAT3",	AT91C_PIN_PA(20), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_A},
+	};
+	pio_configure(sdmmc_pins);
+
+	pmc_enable_periph_clock(CONFIG_SYS_ID_SDHC);
+	pmc_enable_generic_clock(CONFIG_SYS_ID_SDHC,
+				 GCK_CSS_PLLA_CLK,
+				 ATMEL_SDHC_GCKDIV_VALUE);
+#endif
+}
+#endif /* #ifdef CONFIG_SDCARD */
+
+#ifdef CONFIG_NANDFLASH
+void nandflash_hw_init(void)
+{
+	unsigned int reg;
+	const struct pio_desc nand_pins[] = {
+		{"NANDOE", CONFIG_SYS_NAND_OE_PIN, 0, PIO_DEFAULT,
+		 PIO_PERIPH_A},
+		{"NANDWE", CONFIG_SYS_NAND_WE_PIN, 0, PIO_DEFAULT,
+		 PIO_PERIPH_A},
+		{"NANDALE", CONFIG_SYS_NAND_ALE_PIN, 0, PIO_DEFAULT,
+		 PIO_PERIPH_A},
+		{"NANDCLE", CONFIG_SYS_NAND_CLE_PIN, 0, PIO_DEFAULT,
+		 PIO_PERIPH_A},
+		{"NANDCS", CONFIG_SYS_NAND_ENABLE_PIN, 1, PIO_DEFAULT,
+		 PIO_OUTPUT},
+		{"D0", AT91C_PIN_PD(6), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D1", AT91C_PIN_PD(7), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D2", AT91C_PIN_PD(8), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D3", AT91C_PIN_PD(9), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D4", AT91C_PIN_PD(10), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D5", AT91C_PIN_PD(11), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D6", AT91C_PIN_PD(12), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{"D7", AT91C_PIN_PD(13), 0, PIO_DEFAULT, PIO_PERIPH_A},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_A},
+	};
+
+	pio_configure(nand_pins);
+	pmc_enable_periph_clock(AT91C_ID_PIOD);
+
+	reg = readl(AT91C_BASE_SFR + SFR_CCFG_EBICSA);
+	reg |= AT91C_EBI_CS3A_SM | AT91C_EBI_NFD0_ON_D16;
+	reg &= ~AT91C_EBI_DRV;
+	writel(reg, AT91C_BASE_SFR + SFR_CCFG_EBICSA);
+
+	/* Configure SMC CS3 for NAND */
+	writel(AT91C_SMC_NWESETUP_(4), AT91C_BASE_SMC + SMC_SETUP3);
+
+	writel(AT91C_SMC_NWEPULSE_(10) | AT91C_SMC_NCS_WRPULSE_(20) |
+	       AT91C_SMC_NRDPULSE_(10) | AT91C_SMC_NCS_RDPULSE_(20),
+	       AT91C_BASE_SMC + SMC_PULSE3);
+
+	writel(AT91C_SMC_NWECYCLE_(20) | AT91C_SMC_NRDCYCLE_(20),
+	       AT91C_BASE_SMC + SMC_CYCLE3);
+
+	writel(AT91C_SMC_READMODE | AT91C_SMC_WRITEMODE | AT91C_SMC_TDFEN |
+	       AT91_SMC_TDF_(15), AT91C_BASE_SMC + SMC_CTRL3);
+}
+#endif /* #ifdef CONFIG_NANDFLASH */

board/Config.in
@@ -30,6 +30,7 @@ source "board/sama5d2_lpddr2sip_vb/Config.in.board"
 source "board/sama5d2_icp/Config.in.board"
 source "board/sam9x60ek/Config.in.board"
 source "board/sam9x60_ddr2_sip_eb/Config.in.board"
+source "board/sam9x60_sdr_sip_eb/Config.in.board"
 source "contrib/board/Config.in.board"
 
 endchoice
@@ -58,6 +59,7 @@ config CONFIG_BOARDNAME
 	default "sama5d2_icp" if CONFIG_SAMA5D2_ICP
 	default "sam9x60ek" if CONFIG_SAM9X60EK
 	default "sam9x60_ddr2_sip_eb" if CONFIG_SAM9X60_DDR2_SIP_EB
+	default "sam9x60_sdr_sip_eb" if CONFIG_SAM9X60_SDR_SIP_EB
 source "contrib/board/Config.in.boardname"
 
 config AT91SAM9260

board/sam9x60_sdr_sip_eb/Config.in.board
@@ -1,0 +1,18 @@
+config	CONFIG_SAM9X60_SDR_SIP_EB
+	bool "sam9x60_sdr2_sip_eb"
+	select SAM9X60
+	select CONFIG_SDRAM
+	select ALLOW_SDRAM_16BIT
+	select ALLOW_DATAFLASH
+	select ALLOW_NANDFLASH
+	select ALLOW_SDCARD
+	select CONFIG_HAS_HW_INFO
+	select CONFIG_HAS_EEPROM
+	select ALLOW_CPU_CLK_600MHZ
+	select ALLOW_CRYSTAL_24_000MHZ
+	select SUPPORT_BUS_SPEED_200MHZ
+	select CONFIG_FLEXCOM
+	help
+	    Use the SAM9X60 SDR SiP Engineering Board
+	    The important part is the initialization of the SDR SiP
+	    which can be reused for different boards.

board/sam9x60_sdr_sip_eb/Config.in.linux_arg
@@ -1,0 +1,3 @@
+config CONFIG_LINUX_KERNEL_ARG_STRING
+	default "mem=256M console=ttyS0,115200 mtdparts=atmel_nand:8M(bootstrap/kernel)ro,-(rootfs) rw rootfstype=ubifs ubi.mtd=1 root=ubi0:rootfs" if CONFIG_SAM9X60_SDR_SIP_EB && !CONFIG_SDCARD
+	default "mem=256M console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootdelay=2" if CONFIG_SAM9X60_SDR_SIP_EB && CONFIG_SDCARD

board/sam9x60_sdr_sip_eb/board.mk
@@ -1,0 +1,2 @@
+CPPFLAGS += -DCONFIG_SAM9X60_SDR_SIP_EB
+ASFLAGS += -DCONFIG_SAM9X60_SDR_SIP_EB

board/sam9x60_sdr_sip_eb/sam9x60_sdr_sip_eb.h
@@ -1,0 +1,99 @@
+/* ----------------------------------------------------------------------------
+ *         Microchip Microprocessor (MPU) Software Team
+ * ----------------------------------------------------------------------------
+ * Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Microchip's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY MICROCHIP "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL MICROCHIP BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __SAM9X60_SDR_SIP_EB_H__
+#define __SAM9X60_SDR_SIP_EB_H__
+
+/*
+ * PMC Settings
+ *
+ * The main oscillator is enabled as soon as possible in the lowlevel_clock_init
+ * and MCK is switched on the main oscillator.
+ * PLL initialization is done later in the hw_init() function
+ */
+#define MASTER_CLOCK	200000000
+
+#define BAUD_RATE	115200
+#define BOARD_MAINOSC	24000000
+
+#define BOARD_PRESCALER_MAIN_CLOCK	(AT91C_PMC_PRES_CLK | \
+					AT91C_PMC_MDIV_1 | \
+					AT91C_PMC_CSS_MAIN_CLK)
+
+#define BOARD_PRESCALER_PLLA	(AT91C_PMC_PRES_CLK | \
+				AT91C_PMC_MDIV_3 | \
+				AT91C_PMC_CSS_PLLA_CLK)
+
+/*
+ * DataFlash Settings
+ */
+#define CONFIG_SYS_SPI_CLOCK    AT91C_SPI_CLK
+#define CONFIG_SYS_SPI_MODE     SPI_MODE0
+
+#if defined(CONFIG_SPI_BUS0)
+#define CONFIG_SYS_BASE_SPI     AT91C_BASE_SPI0
+#elif defined(CONFIG_SPI_BUS1)
+#define CONFIG_SYS_BASE_SPI     AT91C_BASE_SPI1
+#endif
+
+#if defined(CONFIG_QSPI)
+#define CONFIG_SYS_BASE_QSPI		AT91C_BASE_QSPI0
+#define CONFIG_SYS_BASE_QSPI_MEM	AT91C_BASE_QSPI0_MEM
+#define CONFIG_SYS_ID_QSPI		AT91C_ID_QSPI0
+#endif
+
+/*
+ * NandFlash Settings
+ */
+#define CONFIG_SYS_NAND_BASE            AT91C_BASE_CS3
+#define CONFIG_SYS_NAND_MASK_ALE        (1 << 21)
+#define CONFIG_SYS_NAND_MASK_CLE        (1 << 22)
+
+#define CONFIG_SYS_NAND_OE_PIN		AT91C_PIN_PD(0)
+#define CONFIG_SYS_NAND_WE_PIN		AT91C_PIN_PD(1)
+#define CONFIG_SYS_NAND_ALE_PIN		AT91C_PIN_PD(2)
+#define CONFIG_SYS_NAND_CLE_PIN		AT91C_PIN_PD(3)
+#define CONFIG_SYS_NAND_ENABLE_PIN      AT91C_PIN_PD(4)
+
+#define CONFIG_LOOKUP_TABLE_ALPHA_OFFSET	0xC000
+#define CONFIG_LOOKUP_TABLE_INDEX_OFFSET	0x8000
+
+#define CONFIG_LOOKUP_TABLE_ALPHA_OFFSET_1024	0x18000
+#define CONFIG_LOOKUP_TABLE_INDEX_OFFSET_1024	0x10000
+
+/*
+ * SDHC Settings
+ */
+#ifdef CONFIG_SDHC0
+#define CONFIG_SYS_BASE_SDHC	AT91C_BASE_SDHC0
+#define CONFIG_SYS_ID_SDHC	AT91C_ID_SDMMC0
+#endif
+
+#ifdef CONFIG_SDHC1
+#define CONFIG_SYS_BASE_SDHC	AT91C_BASE_SDHC1
+#define CONFIG_SYS_ID_SDHC	AT91C_ID_SDMMC1
+#endif
+
+#endif /*#ifndef __SAM9X60_SDR_SIP_EB_H__ */

board/sam9x60_sdr_sip_eb/sam9x60_sdr_sip_ebdf_qspi_linux_image_dt_defconfig
@@ -1,0 +1,10 @@
+CONFIG_SAM9X60_SDR_SIP_EB=y
+CONFIG_RAM_8MB=y
+CONFIG_SDRAM_16BIT=y
+CONFIG_SPI_CLK=50000000
+CONFIG_OVERRIDE_CMDLINE=y
+CONFIG_IMG_ADDRESS="0x00200000"
+CONFIG_OF_OFFSET="0x00180000"
+CONFIG_LOAD_LINUX=y
+CONFIG_JUMP_ADDR="0x26F00000"
+CONFIG_DEBUG=y

board/sam9x60_sdr_sip_eb/sam9x60_sdr_sip_ebdf_qspi_uboot_defconfig
@@ -1,0 +1,7 @@
+CONFIG_SAM9X60_SDR_SIP_EB=y
+CONFIG_RAM_8MB=y
+CONFIG_SDRAM_16BIT=y
+CONFIG_SPI_CLK=50000000
+CONFIG_IMG_ADDRESS="0x00040000"
+CONFIG_IMG_SIZE="0x000c0000"
+CONFIG_DEBUG=y

commit 7a2d6bad0b92379d58b16b252eede79d8556eb2e

board: sam9x60_sdr_sip_eb: fix MPDDR bit selected by error
This bit should not be selected for SDRAMC controller.
It is only for DDR memories.

Fixes: 7c62e974cc93 ("board: sam9x60_sdr_sip_eb: introduce SAM9X60 SDR SIP Engineering board")
Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

board/sam9x60_sdr_sip_eb/sam9x60_sdr_sip_eb.c
@@ -108,7 +108,7 @@ static void sdramc_init(void)
 	 * We need to also enable AT91C_EBI_NFD0_ON_D16 . Otherwise the DDR will
 	 * not work if NAND lines have been previously used by RomCode
 	 */
-	reg |= (AT91C_EBI_CS1A | AT91C_EBI_DDR_MP_EN | AT91C_EBI_NFD0_ON_D16);
+	reg |= (AT91C_EBI_CS1A | AT91C_EBI_NFD0_ON_D16);
 	writel(reg, (AT91C_BASE_SFR + SFR_DDRCFG));
 
 	pmc_enable_periph_clock(AT91C_ID_SDRAMC);