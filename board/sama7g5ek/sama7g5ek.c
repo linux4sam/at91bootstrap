4cff34ad5706c184c2bd1aa49d3450412ef0ad2a
Eugen Hristev <eugen.hristev@microchip.com>
2020-01-07 16:09:38 +0200

    board: sama7g5ek: enable debug serial (including PIT/PMC/PIO)
    Enable basic serial. This also requires PMC and PIO.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- board/sama7g5ek/sama7g5ek.c
+++ board/sama7g5ek/sama7g5ek.c
@@ -26,14 +26,63 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "flexcom.h"
 #include "usart.h"
-#include "sama7g5ek.h"
+#include "hardware.h"
+#include "arch/at91_pio.h"
 #include "string.h"
 #include "tzc400-interface.h"
 #include "debug.h"
 #include "umctl2.h"
+#include "gpio.h"
+#include "pmc.h"
+#include "sama7g5ek.h"
+
+static void initialize_serial(void)
+{
+	unsigned int baudrate = 115200;
+
+	usart_init(BAUDRATE(200000000, baudrate));
+}
+
+#if defined CONFIG_FLEXCOM
+static struct at91_flexcom flexcoms[] = {
+	{AT91C_ID_FLEXCOM0, FLEXCOM_USART, AT91C_BASE_FLEXCOM0}, /* BT serial */
+	{AT91C_ID_FLEXCOM1, FLEXCOM_SPI, AT91C_BASE_FLEXCOM1}, /* Mikrobus SPI */
+	{AT91C_ID_FLEXCOM2, FLEXCOM_USART, AT91C_BASE_FLEXCOM2}, /* Unused */
+	{AT91C_ID_FLEXCOM3, FLEXCOM_USART, AT91C_BASE_FLEXCOM3}, /* DBGU */
+};
+
+void board_flexcoms_init()
+{
+	flexcoms_init(flexcoms);
+}
+#endif
+
+#if defined(CONFIG_FLEXCOM)
+unsigned int at91_flexcom3_init(void)
+{
+	const struct pio_desc flx_pins[] = {
+		{"FLX_IO0", CONFIG_SYS_DBGU_TXD_PIN, 0, PIO_DEFAULT, PIO_PERIPH_F},
+		{"FLX_IO1", CONFIG_SYS_DBGU_RXD_PIN, 0, PIO_DEFAULT, PIO_PERIPH_F},
+		{(char *)0, 0, 0, PIO_DEFAULT, PIO_PERIPH_F},
+	};
+
+	pio_configure(flx_pins);
+	pmc_enable_periph_clock(AT91C_ID_FLEXCOM3);
+
+	flexcom_init(3);
+
+	return flexcom_get_regmap(3);
+}
+#endif
 
 void hw_init(void)
 {
+	board_flexcoms_init();
+	at91_flexcom3_init();
+
+	initialize_serial();
+
 	usart_puts("hardware init CA7\n");
 }
