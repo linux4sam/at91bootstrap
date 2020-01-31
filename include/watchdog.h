6eb7b95fe835d75d3297ca09ec8780f9f6453186
Eugen Hristev <eugen.hristev@microchip.com>
2020-01-31 09:35:41 +0200

    driver: wdt2: introduce support for DWDT and WDTS
    DWDT is the dual watchdog timer support in which one watchdog is
    permanently not secure and the other one is programmable secure.
    Introduce CONFIG_WDTS for SoCs that want to tackle this second watchdog.
    Introduce CPU_HAS_DWDT for SoCs that support this feature
    Add support for disabling this watchdog in the wdt2 driver.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- include/watchdog.h
+++ include/watchdog.h
@@ -29,6 +29,9 @@
 #define __WATCHDOG_H__
 
 extern void at91_disable_wdt(void);
+#ifdef CONFIG_WDTS
+extern void at91_disable_wdts(void);
+#endif
 
 #if defined(CONFIG_ENTER_NWD)
 extern unsigned int at91_wdt_set_counter(unsigned int count);
