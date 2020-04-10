fd2579d0ca20deaba362bf547c0333ed21d7c7b4
LI BIN <bin.li@microchip.com>
2020-04-10 17:42:40 +0800

    scripts: generate the 52-word binary header and write it to temp binary file
    2 arguments: dot_config_path and binaries_path
    
    Signed-off-by: LI BIN <bin.li@microchip.com>
    (cherry picked from commit 56607f94a58ea72fd6e6945f41fe99cda9c58fa5)

--- scripts/addpmecchead.py
+++ scripts/addpmecchead.py
@@ -1,41 +1,53 @@
 #!/usr/bin/env python3
 
 import struct, sys
-
+import os
 import pmecc_head
+import re
 
-# open bootstrap file
-fd = open(sys.argv[1], "rb")
-line = fd.read()
-fd.close()
+def find_in_file(file, pattern):
+    if os.path.exists(file):
+        with open(file) as f:
+            result = re.search(pattern, f.read())
+            f.close()
+        return result
 
-if sys.argv[3] == "sama5d2_ptc_ek":
-	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 4, 512)
-elif sys.argv[3].startswith("sama5d3"):
-	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 4, 512)
-elif sys.argv[3] == "sama5d4ek" or sys.argv[3] == "sama5d4_xplained":
-	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
-elif sys.argv[3] == "at91sam9x5ek" or sys.argv[3] == "at91sam9n12ek":
-	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
-elif sys.argv[3] == "sam9x60ek":
-	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
-elif sys.argv[3] == "sam9x60_ddr2_sip_eb":
-	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
-elif sys.argv[3] == "sam9x60_sdr_sip_eb":
-	pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
-elif sys.argv[3] == "core9g25":
-	pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
-else:
-	sys.exit("Not support board!")
+def pmecchead(dot_config_path, binaries_path):
+    header = os.path.join(binaries_path, "pmecc.tmp")
+    if find_in_file(dot_config_path, "CONFIG_NANDFLASH=y"):
+        if not (os.path.exists(binaries_path)):
+            os.mkdir(binaries_path)
+        if find_in_file(dot_config_path, "CONFIG_SAMA5D2_PTC_EK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 4, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAMA5D3XEK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 4, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAMA5D3_XPLAINED=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 4, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAMA5D4EK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAMA5D4_XPLAINED=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
+        elif find_in_file(dot_config_path, "CONFIG_AT91SAM9X5EK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
+        elif find_in_file(dot_config_path, "CONFIG_AT91SAM9N12EK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAM9X60EK=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAM9X60_DDR2_SIP_EB=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(4096, 224, 8, 512)
+        elif find_in_file(dot_config_path, "CONFIG_SAM9X60_SDR_SIP_EB=y"):
+            pmecc_word = pmecc_head.gen_pmecc_header(2048, 64, 2, 512)
+        else:
+            return
+    else:
+        return
 
-vec = struct.pack("<I", pmecc_word)
+    vec = struct.pack("<I", pmecc_word)
+    # generate a new file with pmecc header
+    fd = open(header, "wb")
+    for i in range(0, 52):
+        fd.write(vec)
+    fd.close()
 
-# generate a new file with pmecc header
-fd = open(sys.argv[2], "wb")
-
-for i in range(0, 52):
-	fd.write(vec)
-
-fd.write(line[0:])
-
-fd.close()
+if __name__ == "__main__":
+    pmecchead(sys.argv[1], sys.argv[2])
