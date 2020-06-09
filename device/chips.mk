d3766907d54ca355433c3266d24e233e3586b8f4
Tony Han <tony.han@microchip.com>
2020-06-09 16:00:03 +0800

    Makefile: add CONFIG_ prefix to device/chips.mk
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- device/chips.mk
+++ device/chips.mk
@@ -1,9 +1,9 @@
-ifeq ($(CORE_ARM926EJS), y)
+ifeq ($(CONFIG_CORE_ARM926EJS), y)
 CPPFLAGS += -mcpu=arm926ej-s -mtune=arm926ej-s -mfloat-abi=soft
 ASFLAGS += -mcpu=arm926ej-s -mtune=arm926ej-s -mfloat-abi=soft
 endif
 
-ifeq ($(CORE_CORTEX_A5), y)
+ifeq ($(CONFIG_CORE_CORTEX_A5), y)
 CPPFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
 ASFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
 endif
