13222a3f1e6255a8f1b0039cbed58f386fdefa81
LI BIN <bin.li@microchip.com>
2020-06-22 15:29:34 +0800

    include: new header file include const timing setting
    Signed-off-by: LI BIN <bin.li@microchip.com>

---
+++ include/ddr_device.h
@@ -0,0 +1,163 @@
+/* ----------------------------------------------------------------------------
+ *         ATMEL Microcontroller Software Support
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2020, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __DDR_DEVICE_H__
+#define __DDR_DEVICE_H__
+
+#include "hardware.h"
+#include "arch/at91_ddrsdrc.h"
+#include "ddramc.h"
+
+#if defined(CONFIG_DDR_SET_BY_DEVICE)
+static const struct ddram_timings ddr_ddram_timings = {
+#if defined(CONFIG_DDR_MT41K128M16_D2)
+/* Two DDR3L(MT41H128M16JT-125-K = 16 Mbit x 16 x 8 banks), total 4Gbit on SAMA5D2 Xplained Ultra Evaluation Kit*/
+#if defined(CONFIG_BUS_SPEED_116MHZ)
+	.tras = 5,
+	.trcd = 2,
+	.twr = 4,
+	.trc = 6,
+	.trp = 2,
+	.trrd = 4,
+	.twtr = 4,
+	.tmrd = 4,
+	.trfc = 19,
+	.txsnr = 21,
+	.txsrd = 0,
+	.txp = 10,
+	.txard = 0,
+	.txards = 0,
+	.trpa = 0,
+	.trtp = 4,
+	.tfaw = 5,
+#elif defined(CONFIG_BUS_SPEED_166MHZ)
+	.tras = 6,
+	.trcd = 3,
+	.twr = 4,
+	.trc = 9,
+	.trp = 3,
+	.trrd = 4,
+	.twtr = 4,
+	.tmrd = 4,
+	.trfc = 27,
+	.txsnr = 29,
+	.txsrd = 0,
+	.txp = 3,
+	.txard = 0,
+	.txards = 0,
+	.trpa = 0,
+	.trtp = 4,
+	.tfaw = 7,
+#else
+#error "No CLK setting defined"
+#endif /* Endif of CONFIG_BUS_SPEED_116MHZ */
+#elif defined(CONFIG_DDR_W972GG6KB_D2)
+/* Two DDR2 (W972GG6KB-25-2 Gbits = 16 Mbits x 16 x 8 banks), total 4 Gbits on the SAMA5D2-PTC-EK */
+/* all timings in nCK */
+	.tras = 8,
+	.trcd = 3,
+	.twr = 3,
+	.trc = 10,
+	.trp = 3,
+	.trrd = 4,
+	.twtr = 4,
+	.tmrd = 2,
+	.trfc = 33,
+	.txsnr = 35,
+	.txsrd = 200,
+	.txp = 3,
+	.txard = 2,
+	.txards = 8,
+	.trpa = 4,
+	.trtp = 2,
+	.tfaw = 8,
+#elif defined(CONFIG_DDR_W972GG6KB_9X60)
+/* DDR2 (W972GG6KB-25-2 Gbits = 16 Mbits x 16 x 8 banks), total 4 Gbits on the SAMA5D2-PTC-EK */
+/* all timings in nCK */
+	.tras = 8,
+	.trcd = 3,
+	.twr = 3,
+	.trc = 11,
+	.trp = 3,
+	.trrd = 2,
+	.twtr = 2,
+	.tmrd = 2,
+	.trfc = 26,
+	.txsnr = 28,
+	.txsrd = 200,
+	.txp = 2,
+	.txard = 8,
+	.txards = 2,
+	.trpa = 3,
+	.trtp = 2,
+	.tfaw = 7,
+
+#elif defined(CONFIG_DDR_W971GG6SB_D2)
+/* DDR2 (W971GG6SB = 8 Mwords x 8 Banks x 16 bits), total 1 Gbit in SiP on the SAMA5D2-SOM-EK */
+/* all timings in nCK */
+	.tras = 7,
+	.trcd = 3,
+	.twr = 3,
+	.trc = 9,
+	.trp = 3,
+	.trrd = 2,
+	.twtr = 2,
+	.tmrd = 2,
+	.trfc = 22,
+	.txsnr = 23,
+	.txsrd = 200,
+	.txp = 2,
+	.txard = 2,
+	.txards = 8,
+	.trpa = 4,
+	.trtp = 2,
+	.tfaw = 8,
+#elif defined(CONFIG_DDR_AD220032D)
+/* LPDDR2 (AD220032D = 8 Mwords x 8 Banks x 16 bits), total 3 Gbit in SiP on the SAMA5D2-WLSOM-EK */
+	.tras = 7,
+	.trcd = 3,
+	.twr = 4,
+	.trc = 11,
+	.trp = 4,
+	.trrd = 2,
+	.twtr = 2,
+	.tmrd = 5,
+	.trfc = 21,
+	.txsnr = 0,
+	.txsrd = 23,
+	.txp = 2,
+	.txard = 0,
+	.txards = 0,
+	.trpa = 4,
+	.trtp = 2,
+	.tfaw = 10,
+#else
+#error "DDR-SDRAM device is not supported!"
+#endif /* Endif of  CONFIG_DDR_MT41K128M16_D2 */
+};
+#endif /* Endif of CONFIG_DDR_SET_BY_DEVICE */
+#endif /* Endif __DDR_DEVICE_H__ */
---
+++ include/ddr_jedec.h
@@ -0,0 +1,271 @@
+/* ----------------------------------------------------------------------------
+ *         ATMEL Microcontroller Software Support
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2020, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __DDR_JEDET_H__
+#define __DDR_JEDET_H__
+
+#include "hardware.h"
+#include "arch/at91_ddrsdrc.h"
+#include "ddramc.h"
+
+#if defined(CONFIG_DDR_SET_JEDEC)
+/* Convert nanoseconds to clock cycles for given master clock in MHz */
+#define NS2CYCLES(ns, clk) ((((ns) * (clk)) + 999) / 1000)
+
+#if defined(CONFIG_LPDDR1)
+static const struct ddram_timings ddr_ddram_timings = {
+/* Reference to JESD209B Low Power Double Data Rate (LPDDR) SDRAM Standard */
+#if defined(CONFIG_DDR_SPEED_200)
+	.tras = 50,
+	.trcd = 30,
+	.trp = 30,
+	.trc = 80,
+	.trrd = 15,
+	.twtr = 1,
+#elif defined(CONFIG_DDR_SPEED_266)
+	.tras = 45,
+	.trcd = 22,
+	.trp = 22,
+	.trc = 67,
+	.trrd = 15,
+	.twtr = 1,
+#elif defined(CONFIG_DDR_SPEED_333)
+	.tras = 42,
+	.trcd = 18,
+	.trp = 18,
+	.trc = 60,
+	.trrd = 12,
+	.twtr = 2,
+#elif defined(CONFIG_DDR_SPEED_370)
+	.tras = 42,
+	.trcd = 16,
+	.trp = 16,
+	.trc = 58,
+	.trrd = 11,
+	.twtr = 2,
+#elif defined(CONFIG_DDR_SPEED_400)
+	.tras = 40,
+	.trcd = 15,
+	.trp = 15,
+	.trc = 55,
+	.trrd = 10,
+	.twtr = 2,
+#endif /* Endif of CONFIG_DDR_SPEED_200 */
+	.twr = 15,
+	.tmrd = 2,
+	
+#if defined(CONFIG_DDR_1_GBIT) || defined(CONFIG_DDR_2_GBIT)
+/* 1GB/2GB */
+	.trfc = 140,
+#elif defined(CONFIG_DDR_512_MBIT)
+/* 512MB */
+	.trfc = 110,
+#else
+	.trfc = 80,
+#endif /* Endif of CONFIG_DDR_1_GBIT */
+/*In case of low-power DDR-SDRAM, this field is equivalent to tXSR timing.*/
+	.txsnr = 200,
+	.txsrd = 0,
+	.tfaw = 0,
+	.txard = 0,
+	.txards = 0,
+	.txp = 25,
+	.trtp = 2,
+};
+
+#elif defined(CONFIG_DDR2)
+/* Reference to JEDEC STANDARD JESD79-2F DDR2 SDRAM SPECIFICATION (Revision of JESD79-2E)*/
+static const struct ddram_timings ddr_ddram_timings = {
+#if defined(CONFIG_DDR_SPEED_400)
+	.tras = 40,
+	.trcd = 15,
+	.trp = 15,
+	.trc = 55,
+	.twtr = 10,
+	.txard = 2,
+	.txards = 6,
+#elif defined(CONFIG_DDR_SPEED_533)
+	.tras = 45,
+	.trcd = 12,
+	.trp = 12,
+	.trc = 57,
+	.twtr = 8,
+	.txard = 2,
+	.txards = 6,
+#elif defined(CONFIG_DDR_SPEED_667)
+	.tras = 45,
+	.trcd = 15,
+	.trp = 15,
+	.trc = 60,
+	.twtr = 8,
+	.txard = 2,
+	.txards = 7,
+#elif defined(CONFIG_DDR_SPEED_800)
+	.tras = 45,
+	.trcd = 13,
+	.trp = 13,
+	.trc = 58,
+	.twtr = 8,
+	.txard = 2,
+	.txards = 8,
+#endif #endif /* Endif of CONFIG_DDR_SPEED_400 */
+
+#if defined(CONFIG_DDR_2_GBIT) || defined(CONFIG_DDR_1_GBIT) || defined(CONFIG_DDR_512_MBIT)
+	.trrd = 10,
+#else
+	.trrd = 8,
+#endif #endif /* Endif of CONFIG_DDR_2_GBIT */
+
+	.twr = 15,
+	.tmrd = 2,
+	
+#if defined(CONFIG_DDR_2_GBIT)
+/* 2Gb addressing only support for A0 - A13 */
+	.trfc = 198,
+	.txsnr = 208,
+#elif defined(CONFIG_DDR_1_GBIT)
+/* 1GB */
+	.trfc = 128,
+	.txsnr = 138,
+#elif defined(CONFIG_DDR_512_MBIT)
+/* 512MB */
+	.trfc = 105,
+	.txsnr = 115,
+#else
+	.trfc = 75,
+	.txsnr = 85,
+#endif /* Endif of CONFIG_DDR_2_GBIT */
+	.txsrd = 200,
+	.txp = 2,
+	.trtp = 8,
+
+#if defined(CONFIG_DDR_2_GBIT) || defined(CONFIG_DDR_1_GBIT) || defined(CONFIG_DDR_512_MBIT)
+/* For page size is 2K , page size = 2 ^ COLBITS x ORG !O 8 */
+	#if defined(CONFIG_DDR_SPEED_800)
+		.tfaw = 45,
+	#else
+		.tfaw = 50,
+	#endif /* Endif of CONFIG_DDR_SPEED_800 */
+/* For page size = 1KB , page size = 2 ^ COLBITS x ORG !O 8*/
+#else
+	#if defined(CONFIG_DDR_SPEED_800)
+		.tfaw = 35,
+	#else
+		.tfaw = 38,
+	#endif /* Endif of CONFIG_DDR_SPEED_800 */
+#endif
+};
+
+#elif defined(CONFIG_LPDDR2)
+/* Reference to JEDEC STANDARD JESD209-2F Low Power Double Data Rate 2 (LPDDR2)(Revision of JESD79-2E)*/
+static const struct ddram_timings ddr_ddram_timings = {
+	.tras = 42,
+	.trcd = 15,
+	.trp = 3,
+#if (defined(CONFIG_BANK_4))
+	.trc = 57,
+#else
+	.trc = 60,
+#endif
+	.twtr = 10,
+	.trrd = 10,
+	.twr = 15,
+	/* For low-power DDR2-SDRAM and low-power DDR3-SDRAM, this field is equivalent to TMRW timing.*/
+	.tmrd = 5,
+	
+#if #if defined(CONFIG_DDR_1_GBIT) || defined(CONFIG_DDR_2_GBIT) 
+/* 1Gb | 2Gb */
+	.trfc = 130,
+	.txsnr = 140,
+#else
+	.trfc = 90,
+	.txsnr = 100,
+#endif
+	.txsrd = 0,
+	.txp = 2,
+	.trtp = 8,
+	.tfaw = 60,
+	.txard = 0,
+	.txards = 0,
+};
+
+#elif defined(CONFIG_DDR3)
+/* Reference to JEDEC STANDARD JESD79-3E DDR3 SDRAM Specification(Revision of JESD79-3D, August 2009)*/
+static const struct ddram_timings ddr_ddram_timings = {
+#if defined(CONFIG_DDR_SPEED_800)
+	.tras = 38,
+	.trcd = 13,
+	.trp = 13,
+	.trc = 50,
+	.twtr = 8,
+	.trrd = 10,
+	.twr = 15,
+	.tmrd = 4,
+#elif defined(CONFIG_DDR_SPEED_1066)
+	.tras = 37,
+	.trcd = 12,
+	.trp = 12,
+	.trc = 49,
+	.twtr = 8,
+	.trrd = 10,
+	.twr = 15,
+	.tmrd = 4,
+#elif defined(CONFIG_DDR_SPEED_1333)
+	.tras = 36,
+	.trcd = 11,
+	.trp = 11,
+	.trc = 47,
+	.twtr = 8,
+	.trrd = 8,
+	.twr = 15,
+	.tmrd = 4,
+#else
+#error "No CLK setting defined"
+#endif
+#if defined(CONFIG_DDR_2_GBIT) 
+/* For 2GB */
+	.trfc = 160,
+	/* In case of DDR3-SDRAM, this field is equivalent to tXS timing */
+	.txsnr = 170,
+#elif defined(CONFIG_DDR_1_GBIT)
+/* For 1GB */
+	.trfc = 110,
+	.txsnr = 120,
+#else
+	.trfc = 90,
+	.txsnr = 100,
+#endif
+	.txsrd = 0,
+	.txp = 8,
+	.trtp = 8,
+	.tfaw = 50,
+	.txard = 0,
+	.txards = 0,
+};
+#endif /* Endif of LPDDR1 */
+#endif /* Endif of CONFIG_DDR_SET_JEDEC */
+#endif /* Endif __DDR_JEDET_H__ */
