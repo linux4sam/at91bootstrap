d117c16c99c6b41aceab009f0911e4674c28f8db
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-10-08 18:42:49 +0300

    device: Void return type for matrix_configure_slave()
    There are just reg writes in matrix_configure_slave() and no
    error checking. Change its return type to void.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    [eugen.hristev@microchip.com: port to at91bootstrap4]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- device/sama5d4/sama5d4.c
+++ device/sama5d4/sama5d4.c
@@ -97,7 +97,7 @@ static void initialize_dbgu(void)
 }
 
 #if defined(CONFIG_MATRIX)
-static int matrix_configure_slave(void)
+static void matrix_configure_slave(void)
 {
 	unsigned int ddr_port;
 	unsigned int ssr_setting, sasplit_setting, srtop_setting;
@@ -257,7 +257,6 @@ static int matrix_configure_slave(void)
 					srtop_setting,
 					sasplit_setting,
 					ssr_setting);
-	return 0;
 }
 
 static unsigned int security_ps_peri_id[] = {
@@ -292,14 +291,10 @@ static unsigned int security_ps_peri_id[] = {
 
 static int matrix_init(void)
 {
-	int ret;
-
 	matrix_write_protect_disable(AT91C_BASE_MATRIX64);
 	matrix_write_protect_disable(AT91C_BASE_MATRIX32);
 
-	ret = matrix_configure_slave();
-	if (ret)
-		return -1;
+	matrix_configure_slave();
 
 	return matrix_configure_peri_security(security_ps_peri_id,
 					      ARRAY_SIZE(security_ps_peri_id));
