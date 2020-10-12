784a381e1f9df0361a82bde697d957e1bdd3e110
Tony Han <tony.han@microchip.com>
2020-10-12 13:44:03 +0800

    device: update config files for sama7g5
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- device/chips.mk
+++ device/chips.mk
@@ -7,3 +7,11 @@ ifeq ($(CONFIG_CORE_CORTEX_A5), y)
 CPPFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
 ASFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
 endif
+
+ifeq ($(CONFIG_CORE_CORTEX_A7), y)
+CPPFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7
+ASFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7
+endif
+
+CPPFLAGS +=-DCONFIG_RAM_512MB
+CPPFLAGS += -DMEM_SIZE=0x20000000 -DMEM_BANK=0x60000000
