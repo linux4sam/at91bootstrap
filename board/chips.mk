8eb6c3a9bcadea2dc6b7d8185268fd204f163dbe
Eugen Hristev <eugen.hristev@microchip.com>
2021-03-22 17:05:34 +0200

    board: chips.mk: explicitly select vfp variant
    Explicitly use vfpv4-d16. Some versions of toolchain will automatically
    select NEON (SIMD) for operations, as per the tune for different cortexes.
    NEON has to be explicitly enabled in CP15 of the core before it can be used,
    otherwise it may lead to undefined instruction.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- board/chips.mk
+++ board/chips.mk
@@ -79,12 +79,12 @@ ASFLAGS += -mcpu=arm926ej-s -mtune=arm926ej-s -mfloat-abi=soft
 endif
 
 ifeq ($(CONFIG_CORE_CORTEX_A5), y)
-CPPFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
-ASFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5
+CPPFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5 -mfpu=vfpv4-d16
+ASFLAGS += -mcpu=cortex-a5 -mtune=cortex-a5 -mfpu=vfpv4-d16
 endif
 
 ifeq ($(CONFIG_CORE_CORTEX_A7), y)
-CPPFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7
-ASFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7
+CPPFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=vfpv4-d16
+ASFLAGS += -mcpu=cortex-a7 -mtune=cortex-a7 -mfpu=vfpv4-d16
 endif
 
