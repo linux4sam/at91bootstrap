808abdd0b1d75626fc36e7db164abb6363762f69
Claudiu Beznea <claudiu.beznea@microchip.com>
2021-07-01 15:35:11 +0300

    driver: ddramc: add tim_calr for MT41H128M16JT-125-K
    MT41H128M16JT-125-K is used on SAMA5D2 Xplained. Add tim_calr
    back to avoid any subsequent self-refresh issues.
    
    Fixes: daa4a3fa7bcc9 ("driver: new ddram config script")
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- driver/ddramc.c
+++ driver/ddramc.c
@@ -79,6 +79,7 @@ static void ddram_reg_config(struct ddramc_register *ddramc_config)
 	 * If tref is 7.8us, we have: 400000 / 7.8 = 51282(0xC852)
 	 * */
 	ddramc_config->cal_mr4r = AT91C_DDRC2_COUNT_CAL(0xC852);
+	ddramc_config->tim_calr = AT91C_DDRC2_ZQCS(64);
 #elif defined(CONFIG_DDR_W632GU6MB)
 /* Two DDR3L(W632GU6MB-12 = 16 Mbit x 16 x 8 banks), total 4 Gbit on SAMA5D2 ICP*/
 	type = AT91C_DDRC2_MD_DDR3_SDRAM;
