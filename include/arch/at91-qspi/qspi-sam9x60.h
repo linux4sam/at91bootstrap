a961d1f537106b69f7d03918d8df2df9a6d84a54
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-10-07 16:44:36 +0300

    include: arch: at91-qspi: sam9x60: Add remaining register definitions
    Add sam9x60 QSPI remaining register definitions.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- include/arch/at91-qspi/qspi-sam9x60.h
+++ include/arch/at91-qspi/qspi-sam9x60.h
@@ -10,4 +10,17 @@
 #define	QSPI_WICR	0x34	/* Write Instruction Code Register */
 #define	QSPI_RICR	0x3c	/* Read Instruction Code Register */
 
+#define QSPI_MR_TAMPCLR		(0x1 << 7)	/* Tamper Clear Enable */
+#define QSPI_MR_QICMEN		(0x1 << 13)	/* QSPI Inter-chip Mode Enable */
+
+#define QSPI_IFR_TFRTYP		(0x1 << 12)
+#define QSPI_IFR_DDREN		(0x1 << 15)
+#define QSPI_IFR_APBTFRTYP	(0x1 << 24)
+#define QSPI_IFR_DDRCMDEN	(0x1 << 26)
+
+#define QSPI_SMR_SCRKL		(0x1 << 3)
+
+#define QSPI_WPMR_WPITEN	(0x1 << 1)
+#define QSPI_WPMR_WPCREN	(0x1 << 2)
+
 #endif
